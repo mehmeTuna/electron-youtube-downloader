34cdf01bedd4c85ca01fe18c113c8371407062fc
mehmet <mehmet.tuna@tsoft.com.tr>
2021-02-07 19:53:45 +0300

    initialize app

---
+++ index.js
@@ -0,0 +1,24 @@
+const path = require("path");
+require("electron-reload")(__dirname, {
+  electron: path.join(__dirname, "node_modules", ".bin", "electron"),
+});
+const { app, BrowserWindow, screen } = require("electron");
+
+const createWindow = () => {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  window = new BrowserWindow({
+    width: width / 1.25,
+    height: height / 1.25,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  window.loadFile("public/index.html");
+};
+
+let window = null;
+
+app.whenReady().then(createWindow);
+app.on("window-all-closed", () => app.quit());
